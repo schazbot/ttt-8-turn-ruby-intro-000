d538e7c11f538fd2accf754edb9e454ea48eee0a
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-28 18:01:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  index=input_to_index(user_input)
+  index = input_to_index(user_input)
   user_input = gets.strip
   if valid_move?(board, index)
     move(board, index)
